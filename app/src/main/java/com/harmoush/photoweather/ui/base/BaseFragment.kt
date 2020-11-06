e757d957039f2fb71dda3c4a4aeade29dd27f7df
mohamadKotb <mkotb@identity-solutions.org>
2020-11-06 19:17:48 +0200

    Add Base Fragment and configure MainActivity

---
+++ app/src/main/java/com/harmoush/photoweather/ui/base/BaseFragment.kt
@@ -0,0 +1,28 @@
+package com.harmoush.photoweather.ui.base
+
+import android.os.Bundle
+import androidx.navigation.NavDirections
+import androidx.navigation.fragment.findNavController
+import com.harmoush.photoweather.ui.MainActivity
+import com.phelat.navigationresult.BundleFragment
+
+/*
+Created by Harmoush on 2020-11-06 
+*/
+
+open class BaseFragment : BundleFragment() {
+
+    fun navigate(action: Int, bundle: Bundle? = null) {
+        findNavController().navigate(action, bundle)
+    }
+
+    fun navigate(direction: NavDirections, args: Bundle? = null) {
+        findNavController().navigate(direction.actionId, args)
+    }
+
+    fun navigateUp() {
+        findNavController().navigateUp()
+    }
+
+    fun getMainActivity() = activity as MainActivity?
+}
